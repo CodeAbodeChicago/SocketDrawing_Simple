f0d6e14f6aef0f1955c976f37f4ed91c6d24221b
Michael Hadley <michaelwesthadley@gmail.com>
2015-09-26 14:40:27 -0500

    Remove unneeded module

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
+// _____________________________________________________________________________
+// Setup
+
 // Set up the port
 var port = process.env.PORT || 8080;
 
 // Require the necessary modules
 var path = require("path");
-var random = require("random-js")();
 var express = require('express');
 var app = express();
 var server = app.listen(port);
 var io = require('socket.io')(server);
+
+
+// _____________________________________________________________________________
+// Serving static files
 
 // Serve up the client-side code from public/ to the root (e.g. 127.0.0.1:8080/)
 var publicPath = path.join(__dirname, 'public');
 var staticServer = express.static(publicPath);
 app.use(staticServer);
 
+
+// _____________________________________________________________________________
 // Real time communication
-// In this example, the server is just a relay that passes on information
+// 	In this exercise, the server is just a relay that passes on information
+
 io.on('connection', function (socket) {
 	console.log('a user connected');
 
